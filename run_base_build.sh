60ee5f351d37367841fe035ad2fec2ccae461af2
D Giri <16837707+dgldn@users.noreply.github.com>
2021-08-14 09:38:16 +0100

    Create run_base_build.sh

---
+++ run_base_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "----- Running Base Module ------"
+echo "Starting environment intitialization"
+echo "Starting build process"
+echo "Sharing the build files"
+echo "build completed"
